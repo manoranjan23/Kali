976cd369921702b585114fe40ac4146f1b7f5a5a
manoranjan23 <manoranjanhor43@gmail.com>
2024-12-09 11:19:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/yourtoofan/Zoyu1",
+    "https://github.com/manoranjan23/Kali",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
